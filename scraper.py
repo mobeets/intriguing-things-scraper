9c7137545a158d508b30992d85afc6e3d2fccd0c
mobeets <mobeets@gmail.com>
2015-02-17 13:44:44 -0600

    now called realfuture

--- scraper.py
+++ scraper.py
@@ -8,9 +8,9 @@ from time import mktime
 from dateutil import parser
 from BeautifulSoup import BeautifulSoup
 
-BASE_URL = 'http://tinyletter.com/intriguingthings/letters/'
-RESTART_URL = 'http://tinyletter.com/intriguingthings/letters/5-intriguing-things-like-a-dog-in-an-mri-machine'
-# RESTART_URL = 'http://tinyletter.com/intriguingthings/letters/5-intriguing-things-150'
+BASE_URL = 'http://tinyletter.com/realfuture/letters/'
+RESTART_URL = 'http://tinyletter.com/realfuture/letters/5-intriguing-things-like-a-dog-in-an-mri-machine'
+# RESTART_URL = 'http://tinyletter.com/realfuture/letters/5-intriguing-things-150'
 
 class Thing:
     def __init__(self, dt, number, title, url, src_url):
